4b92842d9ac716ff6d50608d96ff91c716f448d0
vdw <dkrestos@gmail.com>
2014-01-25 21:16:45 +0200

    Update demo page

--- jquery.mapit.js
+++ jquery.mapit.js
@@ -29,30 +29,33 @@ document.write('<scr'+'ipt type="text/javascript" src="https://maps.googleapis.c
 	$.fn.mapit = function(options) {
 
 		var defaults = {
-			latitude: 	37.9792,
-			longitude: 	23.751344,
-			zoom: 			14,
-			type: 			'ROADMAP',
+			latitude: 	 37.970996,
+			longitude: 	 23.730542,
+			zoom: 			 14,
+			type: 			 'ROADMAP',
+			scrollwheel: false,
 			marker: {
-				latitude: 	37.8792,
-				longitude: 	23.751344,
-				icon: 			'images/marker.png',
-				title: 			'Marker title',
+				latitude: 	37.970996,
+				longitude: 	23.730542,
+				icon: 			'images/marker_red.png',
+				title: 			'The Hotel',
 				open: 			false,
 				center: 		true
 			},
-			address: '<h2>The Title</h2><p>Address 1, Area - County<br />Athens 111 11, Greece</p><p>Tel.: +30 210 123 4567<br />Fax: +30 210 123 4567</p>',
+			address: '<h2>The Hotel</h2><p>Address 1, Area - County<br />Athens 123 45, Greece</p><p>Tel.: +30 210 123 4567<br />Fax: +30 210 123 4567</p>',
 			styles: 'GRAYSCALE',
 			locations: [
-				[37.955297, 23.956861, 'images/marker.png', 'Marker title', 'Marker 1 address', false, '1'],
-				[37.975327, 23.853106, 'images/marker.png', 'Marker title', 'Marker 2 address', false, '2'],
-				[38.065798, 23.760481, 'images/marker.png', 'Marker title', 'Marker 3 address', false, '1'],
-				[38.058905, 23.797531, 'images/marker.png', 'Marker title', 'Marker 4 address', false, '2']
+				[37.999252, 23.733353, 'images/marker_blue.png', 'Cinema Aello 5+1', 'Panagi Tsaldari 140, Athina, Greece', false, '1'],
+				[37.996547, 23.732001, 'images/marker_blue.png', 'Cinema Ilion', 'Τροίας 34, Αθήνα, Greece', false, '1'],
+				[37.959408, 23.713982, 'images/marker_green.png', 'Cinema Calipso', 'Megalou Alexandrou ke Kalipsous, Kallithea, Greece', false, '1'],
+				[37.977563, 23.714041, 'images/marker_green.png', 'Technopolis', 'Πειραιώς 100, Αθήνα, Greece', false, '2'],
+				[37.971436, 23.736695, 'images/marker_green.png', 'Zappeion', 'Greece', false, '2'],
+				[37.975030, 23.747751, 'images/marker_green.png', 'Εθνική Πινακοθήκη Μουσείο Αλεξάνδρου Σούτζου', 'Μιχαλακοπούλου 1, Αθήνα, Greece', false, '2'],
+				[37.976104, 23.7141811, 'images/marker_green.png', 'Benaki Museum', 'Koumpari 1, Athens, Greece', false, '2']
 			],
 			origins: [
-				['37.983715', '23.72931'],
-				['37.947091', '23.64261'],
-				['37.943072', '23.950306']
+				['37.936294', '23.947394'],
+				['37.975669', '23.733868']
 			]
 		};
 
@@ -66,6 +69,7 @@ document.write('<scr'+'ipt type="text/javascript" src="https://maps.googleapis.c
 				var directionsDisplay = new google.maps.DirectionsRenderer();
 
 				var mapOptions = {
+					scrollwheel: options.scrollwheel,
 					scaleControl: false,
 					center: 			options.marker.center ? new google.maps.LatLng(options.marker.latitude, options.marker.longitude) : new google.maps.LatLng(options.latitude, options.longitude),
 					zoom: 				options.zoom,
